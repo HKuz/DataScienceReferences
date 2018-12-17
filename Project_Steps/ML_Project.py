345033725b3c9d217db58b11491b020df9e47280
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-12-16 20:42:58 -0500

    Add libraries

--- Project_Steps/ML_Project.py
+++ Project_Steps/ML_Project.py
@@ -1,6 +1,11 @@
 #!/usr/local/bin/python3
 # Code Framework for an End-to-End ML Project
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def main():
     pass
