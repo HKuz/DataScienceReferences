e0f0af642c1466947ce6c7bd06ad1f75a33d8d91
Heather Kusmierz <heather.kusmierz@gmail.com>
2018-12-17 20:09:03 -0500

    Clean up example code, remove unnecessary parts

--- Project_Steps/ML_Project.py
+++ Project_Steps/ML_Project.py
@@ -7,9 +7,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def main():
-    pass
+RANDOM = 42
+np.random.seed(RANDOM)
 
 
-if __name__ == '__main__':
-    main()
+data = pd.DataFrame()
+
+'''
+==================================================
+SPLIT DATA (RANDOM OR STRATIFIED) INTO TRAINING AND TEST SETS
+'''
+
+# 1) Random split
+# from sklearn.model_selection import train_test_split
+# train_set, test_set = train_test_split(data, test_size=0.2,
+#   random_state=RANDOM)
+
+# 2) Stratified split
+# Use one metric to get stratified split
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(data, data['strat_metric']):
+    strat_train_set = data.loc[train_index]
+    strat_test_set = data.loc[test_index]
+
+# Drop the column that was used to stratify the data if manufactured
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop('strat_metric', axis=1, inplace=True)
